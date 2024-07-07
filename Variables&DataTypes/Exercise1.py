978acbb579e2c9c1e7cd6b8ab59ba6256fd5522e
georgeKokkas <geokokkas87@gmail.com>
2024-07-07 18:03:58 +0300

    Add exercises 1 & 2 about Variables & Data Types

---
+++ Variables&DataTypes/Exercise1.py
@@ -0,0 +1,8 @@
+# Create variables for different data types and print their values.
+
+name = "George" # String
+age = 36 # Integer
+height = 1.75 # Float
+isBoy = True # Boolean
+
+print (name, age, height, isBoy) # Output: George 36 1.75 True
---
+++ Variables&DataTypes/Exercise2.py
@@ -0,0 +1,10 @@
+#Convert a float to an integer and a string to a float.
+
+float_value = 20.5
+string_value = "12444.56"
+
+float_to_integer_value = int(float_value)
+string_to_float_value = float(string_value)
+
+print(float_to_integer_value) # Output: 20
+print(string_to_float_value) # Output: 12444.56
