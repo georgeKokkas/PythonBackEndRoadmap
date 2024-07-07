55964c1ab2d230cf2bb9729d0eb2c4662fb22698
georgeKokkas <geokokkas87@gmail.com>
2024-07-07 19:06:53 +0300

    Add exercises 1 & 2 about Control Flow

---
+++ Week1&2-PythonFundamentals/3_ControlFlow/Exercise1.py
@@ -0,0 +1,13 @@
+# Write a program to check if a number is positive, negative, or zero.
+
+
+number = int(input("Give a number :")) # User integer input from terminal
+
+# Conditional statements
+if number > 0:
+    print("Positive")
+elif number == 0:
+    print("Zero")
+else:
+    print("Negative")
+
---
+++ Week1&2-PythonFundamentals/3_ControlFlow/Exercise2.py
@@ -0,0 +1,11 @@
+# Create a loop to print the first 10 natural numbers.
+
+# For Loop
+for i in range(10):
+    print(i) # Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+# While Loop
+count = 0
+while count < 10:
+    print(count)
+    count +=1 # Output: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
