c383a0bb64c5db6e1e62480106416943716d0cc4
georgeKokkas <geokokkas87@gmail.com>
2024-07-08 01:28:48 +0300

    Add exercise 1 about Classed And Objects

---
+++ Week5&6-DataStructures&OOP/4_Classes&Objects/Exercise1.py
@@ -0,0 +1,15 @@
+# Create a class Person with attributes for name and age. Add a method to display a greeting message. Create objects of the Person class and call the greeting messages.
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello, my name is {self.name} and i am {self.age} years old."
+    
+person1 = Person("George", 36)
+person2 = Person("Stacy", 28)
+
+print(person1.greet())
+print(person2.greet())
